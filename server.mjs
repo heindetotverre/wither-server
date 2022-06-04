7a7784553a8d692fc7a01a20d1ca8e7e1664ee21
Matthias <matthias.poortvliet@kpn.com>
2022-06-04 13:38:58 +0200

    changed some server logging

--- server.mjs
+++ server.mjs
@@ -8,6 +8,7 @@ import typedefs from './schemas/index.mjs'
 const app = express()
 const port = process.env.DOKKU_PROXY_PORT || 4000
 const url = ''
+const host = process.env.DOKKU_PROXY_PORT_MAP || `localhost:${port}`
 
 app.use(cors())
 
